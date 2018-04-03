0226720ca9b4dbbdef2258e43dc66ab3efb9ecf2
Takeru Yoshihara <takeru@notainc.com>
2018-04-03 19:19:33 +0900

    test

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ fs.readFile('./john-muir-trail-day-9-乾燥.md', 'utf-8', function (err, text) {
     console.log(err)
   }
     console.log('read ok')
-    fs.writeFile('./john-muir-trail-day-9-乾燥.md', text, function (err) {
+    const newText = text.replace(/mountainboy\.me\/wp-content/g, 'mountainboy.boo.jp/wp-content')
+
+    fs.writeFile('./john-muir-trail-day-9-乾燥.md', newText, function (err) {
       console.log('ok')
     })
 })
