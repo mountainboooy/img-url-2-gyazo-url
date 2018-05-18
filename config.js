33d7c5e97c8859ce226c7f5173a3de3ba3fbad62
Takeru Yoshihara <takeru@notainc.com>
2018-05-19 00:03:54 +0900

    tweak

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  filePath: './export.xml',
-  gyazoAccessToken: '31f273c5c18b6c5f72388719ba7221c3e3a4f66828a5d6a60517bcfe2034094d',
+  filePath: 'ORIGINAL_FILE_PATH',
+  gyazoAccessToken: 'YOUR_GYAZO_ACCESS_TOKEN',
   collectionId: 'img-url-2-gyazo-url'
 }
--- src/practice.js
+++
@@ -1,6 +0,0 @@
-
-
-const str = '[[[https://aaa.jpg]]]http://bbb.jpg]]]'
-const reqExp = /(https||http):\/\/\w*\.(jpg||png)/g
-const result = str.match(reqExp)
-console.log(result)
